d78f75ff3bae4fde2948224f81fd4af07eb1f15e
Alex <alexandrokosm@gmail.com>
2025-12-27 22:15:22 +0200

    [fix] cors error on fetcing from backend

--- backend/api/properties/get_all.php
+++ backend/api/properties/get_all.php
@@ -1,6 +1,13 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5174");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 require_once "../../config/db.php";
 
