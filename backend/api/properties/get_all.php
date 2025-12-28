aba1de454fa088f2c6ad99042bcabe0ffbe5894a
Alex <alexandrokosm@gmail.com>
2025-12-28 18:44:14 +0200

    [configure] get_all.php

--- backend/api/properties/get_all.php
+++ backend/api/properties/get_all.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json");
 
 require_once "../../config/database.php";
