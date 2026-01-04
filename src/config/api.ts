e818fe5f598979d98b7db9c3d3e943231c2646ec
Alex <alexandrokosm@gmail.com>
2026-01-04 18:36:34 +0200

    [discard] previous changes

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
 export const API_BASE =
   import.meta.env.VITE_API_BASE ||
-  "http://dblab.nonrelevant.net/~lab2526omada2/backend/api";
+  "https://dblab.nonrelevant.net/~lab2526omada2/backend/api";
